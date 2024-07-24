1c21b378d04d0b3fa251241d8104322988f3a43a
Mars <Low_style@bk.ru>
2024-07-24 19:34:12 +0500

    Rebuilding 2.1

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,13 +1,13 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserServiceConfig;
 
 import java.security.Principal;
 
-@RestController
+@Controller
 public class UserController {
 
     private final UserServiceConfig userServiceConfig;
